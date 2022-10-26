b173f45eaa570d10f4acac3bf770a0c59a3f6036
ryanpiersma <ryanpiersma131415@gmail.com>
2022-10-26 18:19:27 +0000

    Rewriting ref processing to prepare for full ref

--- src/aligner_prototype.py
+++ src/aligner_prototype.py
@@ -10,41 +10,73 @@ from Crypto.Random import get_random_bytes
 from Crypto.Random import random
 from Crypto.Cipher import AES
 
-ref_bytes = []
+ref_list = []
+ref_loc = []
 
 #Global params to help with debug and test
 debug = False
-limit_hashes = False
-hash_limit = 10
+check_locations = True
+
+limit_hashes = True
+hash_limit = 100
+
+limit_lines = False
+line_limit = 100
 
 def get_ref(ref_file_path):
     print("\nRetrieve reference file at path " + ref_file_path)
-    ref_file = open(ref_file_path, "rb")
-    next(ref_file) #skip first line, i.e. >chr21   
-    ref_bytes = ref_file.read()
-    return ref_bytes
+    
+    ref_file = open(ref_file_path, "r")
+    full_ref = ref_file.readlines() 
+  
+    ref_file.close()
+    return full_ref
 
-def sliding_window_table(key, ref_bytes, read_size=100, hash_bits=15):
-    bytes_read = 0
+def sliding_window_table(key, ref_lines, read_size=100, hash_bits=15):
+
+    #Important stat/debug counters
     hashes_generated = 0
+    lines_processed = 0
+    chrom_counter = 0
+   
+    #Data structure initialization
     hash_buffer = b''
-
     hash_table = [[] for _ in range(2**hash_bits)]
     ref_coords = [[] for _ in range(2**hash_bits)]
-  
-    num_bytes = len(ref_bytes)
 
-    while (len(hash_buffer) < read_size): 
+    end_hashing = False
 
-        if limit_hashes:
-            if hashes_generated > limit_hashes:
-                break
+    while (len(ref_lines) > 0): 
 
-        if bytes_read < num_bytes:
-            hash_buffer += ref_bytes[bytes_read:bytes_read + read_size]
-            bytes_read += read_size 
-        else:
+        if end_hashing:
             break
+
+        if debug:
+            print("Remaining ref lines: " + str(len(ref_lines)))
+
+        load_line = ref_lines.pop(0)[:-1]
+        if debug:
+            print("Line loaded: " + load_line)
+
+        # Detect the start of a new chromosome
+        if ">" in load_line:
+            chrom_counter += 1
+            lines_processed += 1
+            hash_buffer = b''
+            print(">>>Processing next chromosome")
+            continue
+        
+        # Filter out full lines of N bases
+        elif load_line == ('N' * len(load_line)):
+            lines_processed += 1
+            continue
+        
+        # Default case: Load line into hash buffer
+        else:
+            hash_buffer += bytes(load_line, 'utf-8')
+
+        if (hashes_generated == 0) and (len(hash_buffer) >= read_size):
+            print("Chromosome has been filtered. Begin sliding window hash.")
 
         while (len(hash_buffer) >= read_size):
             if debug: 
@@ -53,14 +85,13 @@ def sliding_window_table(key, ref_bytes, read_size=100, hash_bits=15):
             
             newhash = hmac.new(key, hash_buffer[0:read_size], hashlib.sha256)
             curr_hash = newhash.digest()
+           
+            table_index = int.from_bytes(curr_hash, 'big') % (2**hash_bits)
+            bucket_index = len(hash_table[table_index]) 
             
             if debug:
                 print(curr_hash)
-            table_index = int.from_bytes(curr_hash, 'big') % (2**hash_bits)
-            
-            if debug:            
-                print("Placing hash in bucket " + str(table_index))
-            bucket_index = len(hash_table[table_index]) 
+                print("Placing hash in bucket " + str(table_index) + "\n")
            
             hash_table[table_index].append(curr_hash)
             ref_coords[table_index].append(hashes_generated)
@@ -68,9 +99,22 @@ def sliding_window_table(key, ref_bytes, read_size=100, hash_bits=15):
             hashes_generated += 1
             hash_buffer = hash_buffer[1:]          
 
-    print(str(hashes_generated) + " hashes generated\n")
-    return hash_table, ref_coords
+            if limit_hashes and (hashes_generated > hash_limit):
+                end_hashing = True
+                break
+        
+        lines_processed += 1
+        
+        if limit_lines and (lines_processed > line_limit):
+            break
+      
+    print("\n*******************************************")
+    print(str(hashes_generated) + " hashes generated")
+    print(str(lines_processed) + " lines processed") 
+    print(str(chrom_counter) + " chromosome(s) processed")
+    print("*******************************************")
 
+    return hash_table, ref_coords
 
 def find_reads(key, hash_table, ref_coords, hash_bits, filename):
    
@@ -103,11 +147,13 @@ def find_reads(key, hash_table, ref_coords, hash_bits, filename):
                 if hash_table[table_index][i] == curr_hash:
                     if debug: 
                         print("HASH FOUND!")
+                    if check_locations:
                         print("Ref loc = " + str(ref_coords[table_index][i]))
                     find_count += 1
+                    ref_loc.append(ref_coords[table_index][i])                    
                     break
     print(str(read_count) + " reads processed.")
-    print(str(find_count) + "/" + str(read_count) + "READS ALIGNED")
+    print(str(find_count) + "/" + str(read_count) + " READS ALIGNED")
     print(str((float(find_count)/float(read_count)) * 100) + "% of READS ALIGNED\n")
 
 def get_bucket_lens(hash_table, hash_bits):
@@ -171,16 +217,17 @@ def main():
     read_length = 151 #be sure this aligns with your fastq
 
     #Files
-    fasta = "../test_data/chr21_preprocess.fa" 
-    fastq = "../test_data/sim21.fastq"
+    fasta = "../test_data/chr21.fa" 
+    fastq = "../test_data/samples.fq"
    
     #Cloud-side operations   
-    ref_bytes = get_ref(fasta)
+    processed_ref = get_ref(fasta)
+    
     key = get_random_bytes(32)    
-    hash_table, ref_coords = sliding_window_table(key, ref_bytes, read_length ,hash_bits)
+    hash_table, ref_coords = sliding_window_table(key, processed_ref, read_length ,hash_bits)
 
     #Process reads from client side    
-    find_reads(key, hash_table, ref_coords, hash_bits, fastq)  
+    #find_reads(key, hash_table, ref_coords, hash_bits, fastq)  
     
     #Performance measurements
     #make_plots(hash_table, hash_bits, num_samples)
