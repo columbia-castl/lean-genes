2b230c83286ad16250944e8cf92398dba763ab11
ryanpiersma <ryanpiersma131415@gmail.com>
2022-11-01 18:53:15 +0000

    Allow hashing to time itself

--- src/parallelized_hashing.sh
+++ src/parallelized_hashing.sh
@@ -4,5 +4,5 @@ NUM_SPLITS=22
 
 for (( i=1; i<=$NUM_SPLITS; i++ )) 
 do
-	python3 -u aligner_prototype.py ../test_data/GRCh38_split$i.fa > chr_$i.out &
+	time python3 -u aligner_prototype.py ../test_data/GRCh38_split$i.fa > chr_$i.out &
 done
