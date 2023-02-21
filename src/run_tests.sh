1e90a6e71592adb25140ffb3ac1b949fd23c4c0b
ryanpiersma <ryanpiersma131415@gmail.com>
2023-02-21 19:42:06 +0000

    Committing basic runscript for doing sweeps

---
+++ src/run_tests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for PERCENT in 1 2 3 4 5 6 7 8 9
+do
+	echo PERCENT = $PERCENT
+	time python3 aligner_client.py ../test_data/sample-reads/matches_0.$PERCENT.fastq > client_log_$PERCENT
+	cat ../test_data/sample-reads/matches_0.$PERCENT.fastq.sam_* > ../test_data/sample-reads/matches_0.$PERCENT.fastq.sam
+	python3 analysis/sam_stats.py ../test_data/sample-reads/matches_0.$PERCENT.fastq.sam > align_stats_$PERCENT	
+	sleep 30
+done
+
+mkdir run
+mv align_stats_* run
+mv client_log_* run
