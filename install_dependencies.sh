ee2f4e43bbc9d01c7a078227cd36432a21916aa9
ryanpiersma <ryanpiersma131415@gmail.com>
2023-04-07 19:54:45 +0000

    Fix installing script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-git clone https://github.com/lh3/bwa.git
-cd bwa; make
-
-#TODO = Apply PMT patch to BWA!
-
 git clone https://github.com/kokke/tiny-AES-c.git 
+
+git clone https://github.com/lh3/bwa.git
+cp bwa_patch/* bwa
+cd bwa
+git apply --whitespace=fix bwa_pmt.patch
+make
