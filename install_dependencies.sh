2d8c5cf9502dfb4951f611957afc12cd04782ec4
ryanpiersma <ryanpiersma131415@gmail.com>
2023-07-26 08:42:03 +0000

    Commit currently working patch scripts

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,8 +3,9 @@
 git clone https://github.com/kokke/tiny-AES-c.git 
 
 git clone https://github.com/lh3/bwa.git
-cp bwa_patch/* bwa
-cp bwa_patch/read_pmt* src
+#cp bwa_patch/* bwa
+#cp bwa_patch/read_pmt* src
 cd bwa
-git apply bwa_pmt.patch
-make clean; make
+git apply --whitespace=fix --reverse ../bwa_patch/bwa_pmt.patch
+#patch -p1 < bwa_pmt.patch
+make
