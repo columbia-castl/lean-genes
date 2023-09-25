028f8022f9505443aa5e0e488a4e16b4ecbfc263
ryanpiersma <ryanpiersma131415@gmail.com>
2023-09-25 04:17:26 +0000

    Upgrade script for running accuracy experiments

--- src/analysis/calc_accuracy.py
+++ src/analysis/calc_accuracy.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
 
+import os
 import sys
 
-def main():
-    
-    if len(sys.argv) != 3:
-        print("Usage: python3 calc_accuracy.py <SAM file> <threshold>")
-        exit()
+def gather_files(search_dir, ext):
+    found_files = []
+    for root, dirs, files in os.walk(search_dir):
+        for file in files:
+            if (file.split(".")[-1] in ext):
+                found_files.append(file)
 
-    sam_file = open(sys.argv[1], 'r')
-    threshold = int(sys.argv[2])
+    return found_files
+
+def bwa_funnel(fastqs, bwa_path, ref, do_index=False):
+    if do_index:
+        os.system(bwa_path + "/bwa index " + ref)
+
+    for fastq in fastqs:
+        cmd_str = bwa_path + "/bwa mem " + ref + " " + fastq + " > " + fastq.rsplit(".", 1)[0] + ".sam"
+        print(cmd_str)
+        os.system(cmd_str)
+
+def acc_from_sam(sam_name, threshold):
+    
+    sam_file = open(sam_name, 'r')
+    print(sam_name)
 
     num_reads = 0
     correct_reads = 0
@@ -35,6 +50,29 @@ def main():
 
     print("SAM accuracy: ", correct_reads, "/", num_reads)
     print(correct_reads/num_reads * 100, " %")
+
+
+def main():
+    if (len(sys.argv) < 4) or (len(sys.argv) > 5):
+        print("Usage: python3 calc_accuracy.py <FQ directory> <BWA path> <BWA ref> <OPT acc threshold>")
+        exit()
+
+    fastq_dir = sys.argv[1]
+    fastqs = gather_files(fastq_dir, ["fq", "fastq"])
+    print(fastqs)
+
+    bwa_path = sys.argv[2]
+    ref = sys.argv[3]
+    bwa_funnel(fastqs, bwa_path, ref)
+
+    sams = gather_files(fastq_dir, ["sam"])
+    print("SAMs:\n_____")
+    print(sams)
+  
+    if (len(sys.argv) == 5):
+        threshold = int(sys.argv[4])
+        for sam in sams:
+            acc_from_sam(sam, threshold)
 
 if __name__ == "__main__":
     main()
---
+++ src/analysis/multi_wgsim.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import os
+import sys
+
+error_rates = [0.01, 0.02, 0.05, 0.1]
+read_sizes = [150,350,1000]
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python3 multi_wgsim.py <wgsim path> <ref>")
+        exit()
+
+    wgsim_path = sys.argv[1]
+    ref = sys.argv[2]
+
+    for error_rate in error_rates:
+        for read_size in read_sizes:
+            cmd_str = wgsim_path + "/wgsim -e " + str(error_rate) + " -1 " + str(read_size) + " " + ref + " reads_1M_" + str(error_rate) + "_" + str(read_size) + ".fq /dev/null > /dev/null"
+            print(">>> " + cmd_str)
+            os.system(cmd_str)
+
+if __name__ == "__main__":
+    main()
